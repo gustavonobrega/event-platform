b9e1b6ef00ef2a91cc223507ec95b9e2bca7d5e2
Gustavo Nobrega <gustavo.nobrega@hotmail.com>
2022-06-22 16:39:48 -0300

    Update Event page and Video component

--- src/pages/Event.tsx
+++ src/pages/Event.tsx
@@ -1,16 +1,16 @@
-import { gql, useQuery } from "@apollo/client"
 import { Header } from "../components/Header"
-import { Lesson } from "../components/Lesson"
 import { Sidebar } from "../components/Sidebar"
 import { Video } from "../components/Video"
 
 export function Event() {
   return (
-    <div>
+    <div className="flex flex-col min-h-screen">
       <Header />
-      <Sidebar />
-      <Video />
-      <Lesson />
+
+      <main className="flex flex-1">
+        <Video />
+        <Sidebar />
+      </main>
     </div>
   )
 }

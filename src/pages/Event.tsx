b7cbbc03a051d8b8cc9c5a3c53db865255bc932a
Gustavo Nobrega <gustavo.nobrega@hotmail.com>
2022-06-22 12:32:36 -0300

    Create Event page and add theme colors

---
+++ src/pages/Event.tsx
@@ -0,0 +1,18 @@
+import { gql, useQuery } from "@apollo/client"
+import { Header } from "../components/Header"
+import { Lesson } from "../components/Lesson"
+import { Sidebar } from "../components/Sidebar"
+import { Video } from "../components/Video"
+
+export function Event() {
+  return (
+    <div>
+      <Header />
+      <Sidebar />
+      <Video />
+      <Lesson />
+    </div>
+  )
+}
+
+ 

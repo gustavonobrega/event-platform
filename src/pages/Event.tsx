7258d857b014485c8729c1c83d7bd0a0fc612f3c
Gustavo Nobrega <gustavo.nobrega@hotmail.com>
2022-06-23 16:38:31 -0300

    Implement Video

--- src/pages/Event.tsx
+++ src/pages/Event.tsx
@@ -1,14 +1,21 @@
+import { ArrowRight } from "phosphor-react"
+import { useParams } from "react-router-dom"
 import { Header } from "../components/Header"
 import { Sidebar } from "../components/Sidebar"
 import { Video } from "../components/Video"
 
 export function Event() {
+  const { slug } = useParams<{ slug: string }>()
+
   return (
     <div className="flex flex-col min-h-screen">
       <Header />
 
       <main className="flex flex-1">
-        <Video />
+        { slug 
+          ? <Video lessonSlug={slug}/> 
+          : <Video /> 
+        }
         <Sidebar />
       </main>
     </div>
